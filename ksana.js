dd59aebbcb9ce9d8164909818e34fe6066459f34
yapcheahshen <yapcheahshen@gmail.com>
2015-01-15 22:31:31 +0800

    fix ksana.js

--- ksana.js
+++ ksana.js
@@ -1,36 +1,11 @@
 jsonp_handler({
- "version": "1",
- "build": 3,
- "title": "testapp3",
- "minruntime": 4,
- "baseurl": "http:/rawgit.com/ksanaforge/testapp3/master/",
- "description": "",
- "files": [
-  "index.html",
-  "index.css",
-  "bundle.js",
-  "jquery.js",
-  "react-with-addons.js",
-  "ksana.js",
-  "sample.kdb"
- ],
- "filesizes": [
-  552,
-  21,
-  142395,
-  277980,
-  625422,
-  722,
-  97570
- ],
- "filedates": [
-  "2015-01-15T13:45:35.000Z",
-  "2015-01-15T13:45:35.000Z",
-  "2015-01-15T14:11:26.000Z",
-  "2015-01-15T13:45:35.000Z",
-  "2015-01-15T13:45:35.000Z",
-  "2015-01-15T14:10:54.000Z",
-  "2015-01-15T13:45:38.000Z"
- ],
- "date": "2015-01-15T14:11:27.291Z"
+"version": "1",
+"build": 1,
+"title":"testapp3",
+"minruntime": 4,
+"baseurl":"http://rawgit.com/ksanaforge/testapp3/master/",
+"description":"",
+"files":
+  ["index.html","index.css","bundle.js","jquery.js","react-with-addons.js","ksana.js",
+  "bootstrap.min.js","bootstrap.min.css","sample.kdb"]
 })
