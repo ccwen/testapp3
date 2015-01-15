0d9e3bdc233a409f43b7236a8cf179f6ded2b16e
yapcheahshen <yapcheahshen@gmail.com>
2015-01-15 21:45:51 +0800

    'test2'

---
+++ ksana.js
@@ -0,0 +1,11 @@
+jsonp_handler({
+"version": "1",
+"build": 1,
+"title":"testapp3",
+"minruntime": 4,
+"baseurl":"http:/rawgit.com/ksanaforge/testapp3/master/",
+"description":"",
+"files":
+  ["index.html","index.css","bundle.js","jquery.js","react-with-addons.js","ksana.js",
+  "bootstrap.min.js","bootstrap.min.css","sample.kdb"]
+})
